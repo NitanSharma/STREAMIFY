c2aa9781f7d9fa22fa8958c005a90268a0b6773c
Nitin <sharmanitan938@gmail.com>
2025-06-20 16:16:03 +0530

    Done backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -6,10 +8,16 @@ const connectToDB = require('./src/db/db');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./src/routes/user.route');
 const chatRoutes = require('./src/routes/chat.route');
-
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true, // Allow cookies to be sent
+}));
 
 // Middleware to parse JSON
 app.use(express.json());
